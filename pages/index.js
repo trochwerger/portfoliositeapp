efe8abe8c1d6a312bd854a995051d7323da86886
trochwerger <tomasrochwerger3@gmail.com>
2024-05-13 19:52:34 -0400

    Updated background section

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       >
         {/* <RevealOnScroll> */}
         <div className="border rounded">
-          <div className="h-4/6 p-5 ">
+          <div className="p-5">
             <div>
               <h3>Welcome To My Site!</h3>
             </div>
@@ -44,10 +44,146 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <a
+          href="#background-section"
+          className="mx-auto mt-5 text-blue no-underline"
+          onClick={(e) => {
+            e.preventDefault();
+            const yOffset = -100; // Adjust this value according to your header's height
+            const element = document.querySelector("#background-section");
+            const y =
+              element.getBoundingClientRect().top + window.scrollY + yOffset;
+
+            window.scrollTo({ top: y, behavior: "smooth" });
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+            />
+          </svg>
+        </a>
         {/* </RevealOnScroll> */}
       </div>
       {/* <div id="backGroundSection" className="h-dvh m-auto flex  flex-col"> */}
-      <div className="-mt-55 mb-96">
+      <div
+        id="background-section"
+        className="h-dvh m-auto flex justify-center flex-col"
+      >
+        <div className="container mx-auto border rounded flex flex-col">
+          <h3 className="pt-3 pl-2">Projects</h3>
+          <div className="grid grid-cols-3 p-2 gap-3">
+            <div className="col-span-1 ">
+              <Link
+                className="no-underline text-black"
+                target="blank"
+                href={"https://met-collection-app-kap.vercel.app/"}
+              >
+                <div className="border rounded p-2 hover:bg-slate-50">
+                  <Image className="h-48 mx-auto" src="/metAPIpreview.png" />
+                  <p className="text-center pt-2">Metropolitan Museum API</p>
+                </div>
+              </Link>
+            </div>
+            <div className="col-span-1">
+              <Link
+                className="no-underline text-black"
+                target="blank"
+                href={"https://github.com/trochwerger/currencyCounter"}
+              >
+                <div className="border rounded  p-2 hover:bg-slate-50">
+                  <Image
+                    className="h-48 size mx-auto"
+                    src="/currencyCounter.jpg"
+                  />
+                  <p className="text-center pt-2">Currency Counter</p>
+                </div>
+              </Link>
+            </div>
+            <div className="col-span-1">
+              <Link
+                className="no-underline text-black"
+                target="blank"
+                href={"https://github.com/trochwerger/assemblyline/"}
+              >
+                <div className="border rounded p-2 hover:bg-slate-50">
+                  <Image className="h-48 mx-auto" src="/assemblyline.jpg" />
+                  <p className="text-center pt-2">Assembly Line Simulation</p>
+                </div>
+              </Link>
+            </div>
+          </div>
+          <Link
+            href={"https://github.com/trochwerger"}
+            target="blank"
+            className="flex justify-center p-5"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="40"
+              height="40"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+            </svg>
+          </Link>
+        </div>
+        <div className="container border rounded mt-5 grid grid-cols-2">
+          <div className="col-span-1">
+            <h4 className="pt-3">Education</h4>
+            <div className="md:mb-20 mb-10 my-20">
+              <div className="grid grid-cols-3 mt-10">
+                <div className="col-span-1">
+                  <Image className="size-3/6" src="/seneca-logo.svg" />
+                </div>
+                <div className="col-span-1">
+                  <p className="text-sm md:text-base">
+                    Advanced Diploma, Computer Programmer Analyst, Seneca
+                    Polytechnic
+                    <br />
+                    2022-2025
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-1 my-auto bg-slate-400 h-full">
+            <div className="container flex">
+              <div className="w-100 md:p-10">
+                {/* <p>Skills</p>
+          <hr /> */}
+                <div className="grid grid-cols-5 size-8/12 gap-4 mx-auto ">
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original-wordmark.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqldeveloper/sqldeveloper-original.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original-wordmark.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original-wordmark.svg" />
+                  <Image src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="h-dvh mb-96" id="skill-section">
         {/* <RevealOnScroll>
           <RevealRightScroll> */}
         {/* <Skills /> */}
@@ -74,75 +210,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-        {/* </RevealRightScroll> */}
-        <div className="container mx-auto border rounded flex flex-col">
-          <h3 className="pt-3 pl-2">Projects</h3>
-          <div className="grid grid-cols-3 p-2 gap-3">
-            <div className="col-span-1 ">
-              <Link
-                className="no-underline text-black"
-                target="blank"
-                href={"https://met-collection-app-kap.vercel.app/"}
-              >
-                <div className="border rounded  p-2 hover:bg-slate-50">
-                  <Image className="h-48" src="/metAPIpreview.png" />
-                  <p className="text-center pt-2">Metropolitan Museum API</p>
-                </div>
-              </Link>
-            </div>
-            <div className="col-span-1">
-              <Link
-                className="no-underline text-black"
-                target="blank"
-                href={"https://github.com/trochwerger/OOP-Project"}
-              >
-                <div className="border rounded  p-2 hover:bg-slate-50">
-                  <Image className="h-48" src="/assemblyline.jpg" />
-                  <p className="text-center pt-2">Assembly Line Simulation</p>
-                </div>
-              </Link>
-            </div>
-            <div className="col-span-1">
-              <Link
-                className="no-underline text-black"
-                target="blank"
-                href={"https://github.com/trochwerger/OOP-Project"}
-              >
-                <div className="border rounded  p-2 hover:bg-slate-50">
-                  <Image className="h-48" src="/assemblyline.jpg" />
-                  <p className="text-center pt-2">Assembly Line Simulation</p>
-                </div>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="container border rounded">
-          <h4 className="">Education</h4>
-          <div className="md:mb-20 mb-10 my-20">
-            <div className="grid grid-cols-3 mt-10">
-              <div className="col-span-1">
-                <Image className="size-3/6" src="/seneca-logo.svg" />
-              </div>
-              <div className="col-span-1">
-                <p className="text-sm md:text-base">
-                  Advanced Diploma, Computer Programmer Analyst, Seneca
-                  Polytechnic
-                  <br />
-                  2022-2025
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* </RevealOnScroll> */}
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
       </div>
     </>
   );
