ecd3220619043af599ab89777e30515055f77003
trochwerger <tomasrochwerger3@gmail.com>
2024-05-13 21:52:39 -0400

    Updated favicon, fixed esLint warnings

--- components/RevealRightScroll.js
+++ components/RevealRightScroll.js
@@ -1,30 +1,30 @@
-import React, { useEffect, useRef } from "react";
+// import React, { useEffect, useRef } from "react";
 
-export default function RevealScrollRight(props) {
-  const { children } = props;
-  const domRef = useRef();
+// export default function RevealScrollRight(props) {
+//   const { children } = props;
+//   const domRef = useRef();
 
-  useEffect(() => {
-    const element = domRef.current;
+//   useEffect(() => {
+//     const element = domRef.current;
 
-    const startRevealRight = (event) => {
-      if (event.animationName === "fadeIn") {
-        element.classList.remove("animate-fadeInRight");
-        element.classList.add("animate-fadeInRight");
-      }
-      if (event.animationName === "fadeOut") {
-        element.classList.remove("animate-fadeInRight");
-        element.classList.add("animate-fadeOutRight");
-      }
-    };
+//     const startRevealRight = (event) => {
+//       if (event.animationName === "fadeIn") {
+//         element.classList.remove("animate-fadeInRight");
+//         element.classList.add("animate-fadeInRight");
+//       }
+//       if (event.animationName === "fadeOut") {
+//         element.classList.remove("animate-fadeInRight");
+//         element.classList.add("animate-fadeOutRight");
+//       }
+//     };
 
-    element.classList.add("animate-fadeIn");
-    element.addEventListener("animationend", startRevealRight, false);
+//     element.classList.add("animate-fadeIn");
+//     element.addEventListener("animationend", startRevealRight, false);
 
-    return () => {
-      element.removeEventListener("animationend", startRevealRight);
-    };
-  }, []);
+//     return () => {
+//       element.removeEventListener("animationend", startRevealRight);
+//     };
+//   }, []);
 
-  return <div ref={domRef}>{children}</div>;
-}
+//   return <div ref={domRef}>{children}</div>;
+// }
--- components/RevealScroll.js
+++ components/RevealScroll.js
@@ -1,53 +1,53 @@
-import React, { useEffect, useRef } from "react";
+// import React, { useEffect, useRef } from "react";
 
-export default function RevealScroll(props) {
-  const { children } = props;
-  const domRef = useRef();
+// export default function RevealScroll(props) {
+//   const { children } = props;
+//   const domRef = useRef();
 
-  useEffect(() => {
-    const viewportWidth = window.innerWidth;
-    const fadeInThreshold = 0.5;
-    const fadeOutThreshold = 0.9;
-    // const fadeInThreshold = viewportWidth < 600 ? 0.8 : 0.5;
-    // const fadeOutThreshold = viewportWidth < 600 ? 1 : 0.8;
+//   useEffect(() => {
+//     const viewportWidth = window.innerWidth;
+//     const fadeInThreshold = 0.5;
+//     const fadeOutThreshold = 0.9;
+//     // const fadeInThreshold = viewportWidth < 600 ? 0.8 : 0.5;
+//     // const fadeOutThreshold = viewportWidth < 600 ? 1 : 0.8;
 
-    const fadeInObserver = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("animate-fadeIn");
-            entry.target.classList.remove("animate-fadeOut");
-          }
-        });
-      },
-      {
-        rootMargin: "-50px",
-        threshold: fadeInThreshold,
-      }
-    );
+//     const fadeInObserver = new IntersectionObserver(
+//       (entries) => {
+//         entries.forEach((entry) => {
+//           if (entry.isIntersecting) {
+//             entry.target.classList.add("animate-fadeIn");
+//             entry.target.classList.remove("animate-fadeOut");
+//           }
+//         });
+//       },
+//       {
+//         rootMargin: "-50px",
+//         threshold: fadeInThreshold,
+//       }
+//     );
 
-    const fadeOutObserver = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (!entry.isIntersecting) {
-            entry.target.classList.remove("animate-fadeIn");
-            entry.target.classList.add("animate-fadeOut");
-          }
-        });
-      },
-      {
-        rootMargin: "-50px",
-        threshold: fadeOutThreshold,
-      }
-    );
+//     const fadeOutObserver = new IntersectionObserver(
+//       (entries) => {
+//         entries.forEach((entry) => {
+//           if (!entry.isIntersecting) {
+//             entry.target.classList.remove("animate-fadeIn");
+//             entry.target.classList.add("animate-fadeOut");
+//           }
+//         });
+//       },
+//       {
+//         rootMargin: "-50px",
+//         threshold: fadeOutThreshold,
+//       }
+//     );
 
-    fadeInObserver.observe(domRef.current);
-    fadeOutObserver.observe(domRef.current);
-    return () => {
-      fadeInObserver.unobserve(domRef.current);
-      fadeOutObserver.unobserve(domRef.current);
-    };
-  }, []);
+//     fadeInObserver.observe(domRef.current);
+//     fadeOutObserver.observe(domRef.current);
+//     return () => {
+//       fadeInObserver.unobserve(domRef.current);
+//       fadeOutObserver.unobserve(domRef.current);
+//     };
+//   }, []);
 
-  return <div ref={domRef}>{children}</div>;
-}
+//   return <div ref={domRef}>{children}</div>;
+// }
